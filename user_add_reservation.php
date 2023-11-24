702f09d7ff04131f5c8b1707b1ad33a9d5a5f5de
R4tthew <150621054+R4tthew@users.noreply.github.com>
2023-11-24 16:01:11 -0500

    should be able to get the date the user desires

--- user_add_reservation.php
+++ user_add_reservation.php
@@ -45,11 +45,41 @@
         </div>
         <div class="content">
             <h2>Welcome, <?php session_start(); echo $_SESSION['Username_ID']; ?>!</h2>
-            <p>This is where you will decide which method you will use to create your reservation, there are 2 options: 
-            1. We will display all zones that still have available spots, their number of available spots, and the rates for the date you enter.
-            2. We will display the distance between each available zone and the venue you select, alongside the other information listed above.
-            Please decide which method you would like to pursue and use it's respective entry fields and click submit!</p>
+            <p>This is where you will decide which method you will use to create your reservation, there are 2 options: <br>
+            1. We will display all zones that still have available spots, their number of available spots, and the rates for the date you enter. <br>
+            2. We will display the distance between each available zone and the venue you select, alongside the other information listed above. <br>
+            Please decide which method you would like to pursue and use it's respective entry fields and click submit! <br> </p>
         </div>
     </div>
+    
+    <h2>Enter a Date (at least 1 day in advance of the current date)</h2>
+    <form action="process_date.php" method="post">
+        <label for="inputDate">Date:</label>
+        <input type="date" id="inputDate" name="inputDate" required>
+        <button type="submit">Submit</button>
+    </form>
+
+    <?php
+// Validate if the form is submitted
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    
+    // Get the input date from the form
+    $inputDate = $_POST["inputDate"];
+
+    // Validate if the date is at least 1 day in advance
+    $currentDate = date("Y-m-d");
+    $nextDay = date('Y-m-d', strtotime($currentDate . ' +1 day'));
+
+    if ($inputDate < $nextDay) {
+        // Redirect back to the form with an error message
+        header("Location: index.php?error=1");
+        exit();
+    }
+
+    // If the date is valid, redirect to the new page
+    header("Location: new_page_given_date.php?date=" . urlencode($inputDate));
+    exit();
+}
+?>
 </body>
 </html>
