70f7c04e1d12a44e20eec98517255ef7b850e7bb
R4tthew <150621054+R4tthew@users.noreply.github.com>
2023-11-24 15:35:32 -0500

    Created an initial skeleton before adding all functionality

--- user_add_reservation.php
+++ user_add_reservation.php
@@ -1,1 +1,54 @@
-<!-- add later -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Adding a reservation</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 20px;
+            background-color: #f4f4f4;
+        }
+        .container {
+            width: 80%;
+            margin: auto;
+            overflow: hidden;
+        }
+        .header {
+            background: #333;
+            color: #fff;
+            padding: 10px 0;
+            text-align: center;
+        }
+        .nav {
+            padding: 15px;
+            background: #e3e3e3;
+            margin-top: 10px;
+        }
+        .nav a {
+            margin-right: 15px;
+            text-decoration: none;
+            color: #333;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <h1>Adding a reservation</h1>
+        </div>
+        <div class="nav">
+            <a href="user_add_reservation.php">Add a Reservation</a>
+            <a href="user_cancel_reservation.php">Cancel a Reservation</a>
+            <a href="user_reservation_history.php">View Reservation History</a>
+            <a href="user_logout.php">Logout</a> <!-- Placeholder for logout functionality -->
+        </div>
+        <div class="content">
+            <h2>Welcome, <?php session_start(); echo $_SESSION['Username_ID']; ?>!</h2>
+            <p>This is where you may add a reservation, you will see the displayed zones (along with their available spots and relative fees) <br>and 
+              each zone's distance to each venue. To reserve a spot, decide which zone you would like to park in, then enter in the relevant information and click submit!
+            We operate on a cash based system, you will pay in person.</p>
+        </div>
+    </div>
+</body>
+</html>
