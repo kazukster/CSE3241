1a289b6794320cfa117cc5da5ab9c2add9615d4b
R4tthew <150621054+R4tthew@users.noreply.github.com>
2023-11-24 15:15:54 -0500

    Update user_logout.php

--- user_logout.php
+++ user_logout.php
@@ -1,1 +1,13 @@
 <!-- add later --> 
+<?php
+
+// Unset all session variables
+$_SESSION = array();
+
+// Destroy the session
+session_destroy();
+
+// Redirect to the login page after logout
+header("Location: index.php");
+exit;
+?>
