65501187a0c2b8fac0b9914cae423f1cb0a89348
R4tthew <150621054+R4tthew@users.noreply.github.com>
2023-11-27 14:54:49 -0500

    fully functioning add by date

--- user_add_reservation_by_date.php
+++ user_add_reservation_by_date.php
@@ -91,8 +91,58 @@ $selectedDate = $_SESSION['enteredDate'];
             }
 
             echo "</table>";
+
+ 		// Reservation form
+            echo "<h2>Make Reservation</h2>";
+            echo "<form method='post' action=''>
+                    <label for='selected_zone'>Select Zone:</label>
+                    <input type='text' id='selected_zone' name='selected_zone' required>
+                    <br>
+                    <button type='submit'>Make Reservation</button>
+                  </form>";
+
+		$username = $_SESSION['Username_ID'];
+
+		$userPhoneQuery = "SELECT Cellphone FROM users WHERE Username_ID = '$username'";
+           	$userPhoneResult = $conn->query($userPhoneQuery);
+
+            	if ($userPhoneResult->num_rows > 0) {
+                	$userPhoneRow = $userPhoneResult->fetch_assoc();
+                	$userPhone = $userPhoneRow['Cellphone'];
+		}
+
+
+		
+
+            // Process reservation submission
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $selectedZone = $_POST["selected_zone"];
+
+		// Query to get the rate for the selected zone
+                $zoneQuery = "SELECT rate FROM Zones WHERE zone_ID = '$selectedZone'";
+                $zoneResult = $conn->query($zoneQuery);
+
+		if ($zoneResult->num_rows > 0) {
+                    $zoneRow = $zoneResult->fetch_assoc();
+                    $zoneRate = $zoneRow['rate'];
+
+                    // Calculate total fee
+                    $totalFee = $zoneRate;
+                // Insert reservation into the database
+                $insertSql = "INSERT INTO Reservations (user_name, Cellphone, zone_id, event_date, status, total_fee)
+                              VALUES ('$username', '$userPhone', '$selectedZone', '$selectedDate', 1, $totalFee)";
+
+                if ($conn->query($insertSql) === TRUE) {
+                    // Display confirmation number
+                    $confirmationNumber = $conn->insert_id;
+                    echo "<p>Reservation successful! Your confirmation number is: $confirmationNumber, please leave this page!</p>";
+		    } else {
+                    echo "Error: " . $insertSql . "<br>" . $conn->error;
+		}
+                }
+            }
         } else {
-            echo "<p>No available zones for $selectedDate.</p>";
+            echo "<p>No available spots/zones for $selectedDate.</p>";
         }
 
         $conn->close();
