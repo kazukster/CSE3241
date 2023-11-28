4f6762f17dcc3d38be456a52e4f0181a9b43a9d2
cbartolini27 <58894598+cbartolini27@users.noreply.github.com>
2023-11-28 14:24:56 -0600

    Update index.php

--- index.php
+++ index.php
@@ -29,14 +29,14 @@
     <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $servername = "localhost";
-        $dbusername = "phpuser"; // Replace with your DB username
-        $dbpassword = "phpwd"; // Replace with your DB password
-        $dbname = "PARKING_SYSTEM"; // Replace with your DB name
+        $dbusername = "phpuser"; //Database username
+        $dbpassword = "phpwd"; //Database password
+        $dbname = "PARKING_SYSTEM"; //Database name
 
-        // Create database connection
+        //Creating a database connection
         $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 
-        // Check connection
+        //Checking the connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
         }
@@ -45,23 +45,23 @@
             $username_id = $conn->real_escape_string($_POST['username_id']);
             $password = $_POST['password'];
         
-            // Check for admin login
+            //Checking for the admin login
             if ($username_id === 'admin' && $password === 'admin123') {
-                // Redirect to admin-specific page
+                //Redirect to admin-specific page
                 header("Location: admin_page.php");
                 exit;
             } else {
-                // For regular users, check against the database
+                //Check against the database for regular users
                 $sql = "SELECT * FROM users WHERE Username_ID = '$username_id' AND Password = '$password'";
                 $result = $conn->query($sql);
         
                 if ($result->num_rows > 0) {
 
-                    // Set session variable
+                    //Set the session variable
                     session_start();
                     $_SESSION['Username_ID'] = $username_id;
                     
-                    // Redirect to a user-specific page
+                    //Redirect the user page
                     header("Location: user_page.php");
                     exit;
                 } else {
@@ -73,19 +73,19 @@
             $password = $_POST['password_signup'];
             $password_confirm = $_POST['password_confirm'];
         
-            // Check if the cellphone key exists in the POST data
+            //Check if the cellphone key exists in the POST data
             if (!isset($_POST['cellphone_signup'])) {
                 echo "Cellphone number is required.";
-                exit; // Stop the script if the cellphone number is not provided
+                exit; //Stop if the cellphone number is not provided
             }
         
             $cellphone = $conn->real_escape_string($_POST['cellphone_signup']);
 
-            // Check if the two passwords match
+            //Check if the passwords match
             if ($password !== $password_confirm) {
                 echo "Passwords do not match.";
             } else {
-                // Check if username or cellphone already exists
+                //Checks if username or cellphone already exists
                 $checkUser = $conn->prepare("SELECT * FROM users WHERE Username_ID = ? OR Cellphone = ?");
                 $checkUser->bind_param("ss", $username_id, $cellphone);
                 $checkUser->execute();
@@ -93,7 +93,7 @@
                 if ($result->num_rows > 0) {
                     echo "Username or cellphone already exists.";
                 } else {
-                    // Insert new user into the database
+                    //Inserts new user into the database
                     $insertUser = $conn->prepare("INSERT INTO users (Username_ID, Cellphone, Password) VALUES (?, ?, ?)");
                     $insertUser->bind_param("sss", $username_id, $cellphone, $password); // In a real-world scenario, password should be hashed
                     if ($insertUser->execute()) {
