1a8b9dd3aad559018a8d927b9d25ca66d9f1ca87
cbartolini27 <58894598+cbartolini27@users.noreply.github.com>
2023-11-27 20:59:24 -0600

    Update (modified)cancel_reservation.php

--- (modified)cancel_reservation.php
+++ (modified)cancel_reservation.php
@@ -1,4 +1,56 @@
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+!DOCTYPE html>
+<html>
+<head>
+    <title>Cancel a Reservation</title>
+     body {
+            font-family: Arial, sans-serif;
+            margin: 0;
+            padding: 20px;
+            background-color: #f4f4f4;
+        }
+        .container {
+            width: 80%;
+            margin: auto;
+            overflow: hidden;
+        }
+        .header {
+            background: #333;
+            color: #fff;
+            padding: 10px 0;
+            text-align: center;
+        }
+        .nav {
+            padding: 15px;
+            background: #e3e3e3;
+            margin-top: 10px;
+        }
+        .nav a {
+            margin-right: 15px;
+            text-decoration: none;
+            color: #333;
+        }
+</head>
+<body>
+<div class="container">
+    <div class="header">
+        <h1>Cancel a Reservation</h1>
+    </div>
+    <div class="nav">
+           <a href="user_add_reservation.php">Add a Reservation</a>
+            <a href="user_cancel_reservation.php">Cancel a Reservation</a>
+            <a href="user_reservation_history.php">View Reservation History</a>
+            <a href="user_logout.php">Logout</a>
+    </div>
+    <div class="content">
+        <h2>Enter the Confirmation Number to Cancel Your Reservation</h2>
+        <form method="post" action="user_cancel_reservation.php">
+            <label for="confirmation_number">Confirmation Number:</label>
+            <input type="text" id="confirmation_number" name="confirmation_number" required>
+            <button type="submit">Cancel Reservation</button>
+        </form>
+
+        <?php
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $servername = "localhost"; // Replace with your server details
             $username = "phpuser"; // Your DB username
             $password = "phpwd"; // Your DB password
@@ -40,7 +92,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     if ($stmtCancel->execute()) {
                         echo "<p>Reservation cancelled successfully.</p>";
                     } else {
-                        echo "<p>Error cancelling reservation: " . $stmtCancel->error . "</p>";
+                        echo "<p>Error canceling reservation: " . $stmtCancel->error . "</p>";
                     }
 
                     $stmtCancel->close();
@@ -55,3 +107,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             $conn->close();
         }
         ?>
+    </div>
+</div>
+</body>
+</html>
